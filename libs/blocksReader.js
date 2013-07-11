3ddaea8aac44adec5dd65a9416badb8eca90fbf4
A. Akbar Hidayat <keripix@gmail.com>
2013-07-11 14:29:48 +0700

    parseLine depends on block configuration

--- libs/blocksReader.js
+++ libs/blocksReader.js
@@ -1,34 +1,44 @@
 module.exports = Reader;
 
-function Reader(canvas){
-  this.canvas = canvas;
-  this.ctx = canvas.getContext("2d");
-
-  this.canvasWidth = canvas.width;
-  this.canvasHeight = canvas.height;
-
-  this.results = [];
+function Reader(conf){
+  this.blockWidth = conf.blockWidth;
+  this.blockHeight = conf.blockHeight;
 }
 
-Reader.prototype.read = function() {
+Reader.prototype.read = function(canvas) {
+  var results = [],
+      detected,
+      ctx = canvas.getContext("2d"),
+      canvasWidth = canvas.width;
+
   // we will asume that each block has a width and height of 10
-  for (var i = 0, n = this.canvasHeight; i < n; i += 10) {
-    this.results.push = this.parseLine(this.ctx.getImageData(0, 10, this.canvasWidth, this.canvasHeight).data);
+  for (var i = 0, n = canvas.height; i < n; i += 10) {
+    detected = this.parseLine(ctx.getImageData(0, i, canvasWidth, i + 10).data, i / 10);
+
+    if (detected.length){
+      results.push(detected);
+    }
   }
+
+  return results;
 };
 
 Reader.prototype.parseLine = function(data, line) {
-  var detected = [];
+  var detected = [],
+      i = 0,
+      n = data.length;
 
-  for (var i = 0, n = data.length; i < n; i += 4) {
+  while( i < n) {
     if (data[i] === 0){
       detected.push({
-        x: i * 10 / 4,
-        y: line * 10,
-        width: 10,
-        height: 10
+        x: i * this.blockWidth / 4,
+        y: line * this.blockHeight,
+        width: this.blockWidth,
+        height: this.blockHeight
       });
     }
+
+    i += 4*this.blockWidth;
   }
 
   return detected;
--- tests/blocksReaderSpec.js
+++ tests/blocksReaderSpec.js
@@ -1,14 +1,16 @@
 var Reader = require("./../libs/blocksReader"),
     Canvas = require("canvas"),
-    reader,
     canvas,
     blocks;
 
 function drawBlocks(canvas, blocks){
   var ctx = canvas.getContext("2d");
+  // draw the background color first
+  ctx.fillStyle = "#F1C40F";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+  ctx.fillStyle = "#000000";
   blocks.forEach(function(b){
-    ctx.fillStyle = "#000000";
     ctx.fillRect(b.x, b.y, b.width, b.height);
   });
 }
@@ -16,7 +18,6 @@ function drawBlocks(canvas, blocks){
 describe("Reading Blocks", function(){
   beforeEach(function(){
     canvas = new Canvas(50, 50);
-    reader = new Reader(canvas);
     blocks = [
       {x: 0, y: 0, width: 10, height: 10},
       {x: 25, y: 0, width: 10, height: 10},
@@ -26,16 +27,10 @@ describe("Reading Blocks", function(){
       {x: 0, y: 40, width: 10, height: 10},
       {x: 10, y: 40, width: 10, height: 10}
     ];
-
     drawBlocks(canvas, blocks);
   });
 
-  it("Should calculate canvas property", function(){
-    expect(reader.canvasWidth).toEqual(50);
-    expect(reader.canvasHeight).toEqual(50);
-  });
-
-  it("Should Parse each line correctly", function(){
+  it("Should Parse each line correctly with blockWidth = 1", function(){
     var data = [
       0,0,0,0,
       1,1,1,1,
@@ -45,17 +40,39 @@ describe("Reading Blocks", function(){
       0,0,0,0
     ];
 
-    var detected = reader.parseLine(data, 1);
-    expect(detected[0]).toEqual({x: 0, y: 10, width: 10, height: 10});
-    expect(detected[1]).toEqual({x: 20, y: 10, width: 10, height: 10});
-    expect(detected[2]).toEqual({x: 50, y: 10, width: 10, height: 10});
+    var reader = new Reader({blockWidth:1,blockHeight:1}),
+        detected = reader.parseLine(data, 1);
+
+    expect(detected[0]).toEqual({x: 0, y: 1, width: 1, height: 1});
+    expect(detected[1]).toEqual({x: 2, y: 1, width: 1, height: 1});
+    expect(detected[2]).toEqual({x: 5, y: 1, width: 1, height: 1});
 
     expect(detected.length).toEqual(3);
   });
 
-  // it("Should detect the blocks", function(){
-  //   var detected = reader.read();
+  // it("Should Parse each line correctly with blockWidth = 2", function(){
+  //   var data = [
+  //     0,0,0,0,0,0,0,0,
+  //     1,1,1,1,1,1,1,1,
+  //     1,1,1,1,1,1,1,1,
+  //     1,2,3,0,1,2,3,0,
+  //     5,5,5,5,5,5,5,5,
+  //     0,0,0,0,0,0,0,0
+  //   ];
 
+  //   var reader = new Reader({blockWidth:2}),
+  //       detected = reader.parseLine(data, 1);
+
+  //   expect(detected[0]).toEqual({x: 0, y: 10, width: 10, height: 10});
+  //   expect(detected[1]).toEqual({x: 20, y: 10, width: 10, height: 10});
+  //   expect(detected[2]).toEqual({x: 50, y: 10, width: 10, height: 10});
+
+  //   expect(detected.length).toEqual(3);
+  // });
+
+  // it("Should detect the blocks", function(){
+  //   var reader = new Reader({blockWidth:10}),
+  //       detected = reader.read(canvas);
   //   expect(detected).toEqual(blocks);
   // });
 });
