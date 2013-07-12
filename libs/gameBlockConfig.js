ae1e795bb8c62d17f5fc3856b6abe02aa07cc408
A. Akbar Hidayat <keripix@gmail.com>
2013-07-12 09:41:16 +0700

    rename blockColor -> blocksColor

--- libs/gameBlockConfig.js
+++ libs/gameBlockConfig.js
@@ -8,7 +8,7 @@ module.exports = GameBlockConfig;
 function GameBlockConfig(conf){
   conf = conf || {};
 
-  this.blockColor = conf.blockColor || "#010101";
+  this.blocksColor = conf.blocksColor || "#010101";
   this.actorColor = conf.actorColor || "#ECF0F1";
   this.exitColor = conf.exitColor || "#27AE60";
   this.keyColor = conf.keyColor || "#E74C3C";
@@ -16,7 +16,10 @@ function GameBlockConfig(conf){
   this.blockWidth = conf.blockWidth || 10;
   this.blockHeight = conf.blockHeight || 10;
 
+  this.paintedBlocks = [];
+
   this.activeColor = this.blockColor;
+  this.activeKey = "blocksColor";
 }
 
 GameBlockConfig.prototype.setActiveColor = function(name) {
@@ -25,6 +28,7 @@ GameBlockConfig.prototype.setActiveColor = function(name) {
   }
 
   this.activeColor = this[name];
+  this.activeKey = name;
 };
 
 /**
@@ -33,4 +37,8 @@ GameBlockConfig.prototype.setActiveColor = function(name) {
  */
 GameBlockConfig.prototype.getActiveColor = function() {
   return this.activeColor;
+};
+
+GameBlockConfig.prototype.addBlock = function(color) {
+  color = color || this.activeKey;
 };
